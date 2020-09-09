4ba52b731f90bea7683666732c367b4277d490c8
Wechuli <wechulipaul@yahoo.com>
2020-09-09 20:14:23 +0300

    add jsonwebtoken module

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,4 +6,12 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const app = new App({ id: APP_ID, privateKey: PRIVATE_KEY });
 const appJWT = app.getSignedJsonWebToken();
 
+
+class AppAuth{
+    constructor(){
+
+    }
+    
+}
+
 module.exports = { app, appJWT };
